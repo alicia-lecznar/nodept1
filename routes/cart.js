7b73e8c3800db6d558fa5ab5d33448205e855e3d
alicia <alicia.lecznar@gmail.com>
2019-08-26 09:08:11 -0400

    final

---
+++ routes/cart.js
@@ -0,0 +1,23 @@
+const cart = [
+
+    {
+        product: "Off-White Sandals",
+        id: "88qsxa",
+        price: 450,
+        quantity: 1,
+    },
+    {
+        product: "Giuseppe Stiletto",
+        id: "owie8",
+        price: 630,
+        quantity: 2,
+    },
+    {
+        product: "Prada Mules",
+        id: "l033",
+        price: 550,
+        quantity: 6,
+    }
+];
+
+module.exports = cart;
